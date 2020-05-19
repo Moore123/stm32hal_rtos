7eff9dbf2a07ca29ddf513672e7cb3708ebd75f0
Moore123 <lyxmoo@msn.com>
2020-05-19 23:16:52 +0800

    rm empty redundant file

--- rtos_2/led2/main.h
+++ rtos_2/led2/main.h
@@ -1,48 +1,5 @@
 /**
   ******************************************************************************
-  * @file           : main.h
-  * @brief          : Header for main.c file.
-  *                   This file contains the common defines of the application.
-  ******************************************************************************
-  * This notice applies to any and all portions of this file
-  * that are not between comment pairs USER CODE BEGIN and
-  * USER CODE END. Other portions of this file, whether 
-  * inserted by the user or by software development tools
-  * are owned by their respective copyright owners.
-  *
-  * Copyright (c) 2018 STMicroelectronics International N.V. 
-  * All rights reserved.
-  *
-  * Redistribution and use in source and binary forms, with or without 
-  * modification, are permitted, provided that the following conditions are met:
-  *
-  * 1. Redistribution of source code must retain the above copyright notice, 
-  *    this list of conditions and the following disclaimer.
-  * 2. Redistributions in binary form must reproduce the above copyright notice,
-  *    this list of conditions and the following disclaimer in the documentation
-  *    and/or other materials provided with the distribution.
-  * 3. Neither the name of STMicroelectronics nor the names of other 
-  *    contributors to this software may be used to endorse or promote products 
-  *    derived from this software without specific written permission.
-  * 4. This software, including modifications and/or derivative works of this 
-  *    software, must execute solely and exclusively on microcontroller or
-  *    microprocessor devices manufactured by or for STMicroelectronics.
-  * 5. Redistribution and use of this software other than as permitted under 
-  *    this license is void and will automatically terminate your rights under 
-  *    this license. 
-  *
-  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
-  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
-  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
-  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
-  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
-  * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
-  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
-  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   *
   ******************************************************************************
   */
@@ -73,12 +30,40 @@
 
 /* USER CODE END Private defines */
 
+#ifndef STM32F103xB
+    #define  STM32F103xB
+#endif
+
 #ifdef __cplusplus
  extern "C" {
 #endif
-void _Error_Handler(char *, int);
+//extern void _Error_Handler(char *, int);
 
+#define EEPROM_CS_Pin GPIO_PIN_0
+#define EEPROM_CS_GPIO_Port GPIOB
+#define EEPROM_WP_Pin GPIO_PIN_1
+#define EEPROM_WP_GPIO_Port GPIOB
+#define EEPROM_HOLD_Pin GPIO_PIN_10
+#define EEPROM_HOLD_GPIO_Port GPIOB
 #define Error_Handler() _Error_Handler(__FILE__, __LINE__)
+
+#include "stm32f1xx_hal_conf.h"
+#include "stm32f1xx_hal.h"
+#include "stm32f1xx_hal_rcc.h"
+#include "stm32f1xx_hal_rtc.h"
+#include "stm32f1xx_hal_gpio.h"
+#include "stm32f1xx_hal_dma.h"
+#include "stm32f1xx_ll_dma.h"
+
+#include "stm32f1xx_ll_utils.h"
+#include "stm32f1xx_hal_i2c.h"
+
+//extern uint32_t SystemCoreClock;
+
+//= 72000000U;
+
+//#endif
+
 #ifdef __cplusplus
 }
 #endif
